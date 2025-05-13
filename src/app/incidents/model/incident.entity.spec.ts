0d86a8f0dca6ec663eceb8f2d429b4b24cd25b20
ernesto0710 <166081803+ernesto0710@users.noreply.github.com>
2025-05-13 18:53:17 -0500

    fix incident.entity.spec.ts

--- src/app/incidents/model/incident.entity.spec.ts
+++ src/app/incidents/model/incident.entity.spec.ts
@@ -1,7 +1,1 @@
-import { IncidentEntity } from './incident.entity';
 
-describe('IncidentEntity', () => {
-  it('should create an instance', () => {
-    expect(new IncidentEntity()).toBeTruthy();
-  });
-});
