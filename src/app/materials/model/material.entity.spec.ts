0823e28d1cbfad4c3be3030a83fa5f4ec78436f3
DanieGF1 <gomezfloresdanielaaraceli@gmail.com>
2025-05-13 18:24:00 -0500

    feat: add material model

---
+++ src/app/materials/model/material.entity.spec.ts
@@ -0,0 +1,7 @@
+import { MaterialEntity } from './material.entity';
+
+describe('MaterialEntity', () => {
+  it('should create an instance', () => {
+    expect(new MaterialEntity()).toBeTruthy();
+  });
+});
