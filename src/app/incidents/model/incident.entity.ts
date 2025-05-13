279832d2c9fa5db703379e40c57574f2e0a44f7e
ernesto0710 <166081803+ernesto0710@users.noreply.github.com>
2025-05-13 18:55:27 -0500

    fix: incident.entity.ts

--- src/app/incidents/model/incident.entity.ts
+++ src/app/incidents/model/incident.entity.ts
@@ -5,8 +5,9 @@ export class Incident {
   severity: string;
   status: string;
   project_id: number;
+  quantity: number;
 
-  constructor(incident:{incident_id?: number, date?: string, incident_type?: string, severity?: string, status?: string, project_id?: number}) {
+  constructor(incident:{incident_id: number, date?: string, incident_type: string, severity?: string, status?: string, project_id?: number}) {
     this.incident_id = incident.incident_id || 0;
     this.date = incident.date || "";
     this.incident_type = incident.incident_type || "";
