b75b070df906752484099f88f59aa61793e69baa
Paulu27 <139929819+Paulu27@users.noreply.github.com>
2025-05-13 18:17:07 -0500

    feat: Delete src/app/materials/model/material.entity.spec.ts

--- src/app/materials/model/material.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MaterialEntity } from './material.entity';
-
-describe('MaterialEntity', () => {
-  it('should create an instance', () => {
-    expect(new MaterialEntity()).toBeTruthy();
-  });
-});
