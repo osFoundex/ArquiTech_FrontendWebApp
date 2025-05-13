5c12e342bbba7abd3d403dbc9404257e05927dcd
Melisa Geraldine Sulca Silva <u202224602@upc.edu.pe>
2025-05-13 18:58:59 -0500

    fix project.entity.spec.ts

--- src/app/projects/model/project.entity.spec.ts
+++ src/app/projects/model/project.entity.spec.ts
@@ -1,6 +1,7 @@
 import { ProjectEntity } from './project.entity';
+import { MaterialEntity } from '../material/model/material.entity'
 
-describe('ProjectEntity', () => {
+describe('ProjectEntity', 'MaterialEntity' () => {
   it('should create an instance', () => {
     expect(new ProjectEntity()).toBeTruthy();
   });
