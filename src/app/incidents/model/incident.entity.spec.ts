36e011232d52f3a48f392f9160dde6f7a19e39be
Melisa Geraldine Sulca Silva <u202224602@upc.edu.pe>
2025-05-13 18:54:24 -0500

    feat: add incident.entity.spec.ts

--- src/app/incidents/model/incident.entity.spec.ts
+++ src/app/incidents/model/incident.entity.spec.ts
@@ -1,1 +1,7 @@
+import { IncidentEntity } from './incident.entity';
 
+describe('IncidentEntity', () => {
+  it('should create an instance', () => {
+    expect(new IncidentEntity()).toBeTruthy();
+  });
+});
