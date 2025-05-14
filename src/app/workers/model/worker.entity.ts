d8e0230fa70a77cdb237f64d6a508e289d39a227
raulbellidosalas <u202310931@upc.edu.pe>
2025-05-14 06:13:36 -0500

    feat: added worker.entity.ts

--- src/app/workers/model/worker.entity.ts
+++ src/app/workers/model/worker.entity.ts
@@ -1,2 +1,15 @@
-e
+export class Worker {
+  worker_id: number;
+  name: string;
+  role: string;
+  hired_date: string;
+  project_id: number;
 
+  constructor(worker:{worker_id?: number, name?: string, role?: string, hired_date?: string, project_id?: number}) {
+    this.worker_id = worker.worker_id || 0;
+    this.name = worker.name || "";
+    this.role = worker.role || "";
+    this.hired_date = worker.hired_date || "";
+    this.project_id = worker.project_id || 0;
+  }
+}
