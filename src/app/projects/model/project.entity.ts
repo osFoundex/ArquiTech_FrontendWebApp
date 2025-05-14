98f93ba20659c968670bee1a3d662ed0f8f84239
ernesto0710 <166081803+ernesto0710@users.noreply.github.com>
2025-05-13 19:01:21 -0500

    feat: add project.entity.ts

--- src/app/projects/model/project.entity.ts
+++ src/app/projects/model/project.entity.ts
@@ -1,13 +1,12 @@
 export class Project {
   project_id: number;
   name: string;
-  project_name: string;
   user_id: number;
-  user_name: string; 
+  budget: number;
   image_url: string;
   status:string;
 
-  constructor(project:{project_id?: number, name?: string, project_id: number, user_id?: number, budget?: number, image_url?: string, status?:string}) {
+  constructor(project:{project_id?: number, name?: string, user_id?: number, budget?: number, image_url?: string, status?:string}) {
     this.project_id = project.project_id || 0;
     this.name = project.name || "";
     this.user_id = project.project_id || 0;
